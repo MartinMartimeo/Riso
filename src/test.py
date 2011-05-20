1fa022a0116a1d2cb7af2ad66877cde64241ad8a
MartinMartimeo <martin@martimeo.de>
2011-05-20 14:47:50 +0200

    Threaded Sockets

--- src/test.py
+++ src/test.py
@@ -1,3 +1,4 @@
+import socket
 # -*- coding: utf-8 -*-
 # Code By Severin Orth (MartinMartimeo)
 # martin@martimeo.de
@@ -6,13 +7,13 @@
 __author__  = "Severin <MartinMartimeo> Orth <martin@martimeo.de>"
 __date__    = "$20.05.2011 13:33:50$"
 
-import asyncore
 import logging
+import sys
 
-from network.custom_socket import CustomSocket, socket_map
+from network.custom_socket import CustomSocket
 
 # Testing
 
-logging.basicConfig(level=logging.DEBUG)
-CustomSocket('mg.mud.de', 23)
-asyncore.loop(map=socket_map)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s %(threadName)-10s %(levelname)-8s %(message)s')
+socket = CustomSocket('mg.mud.de', 23)
+socket.threaded_connect()
