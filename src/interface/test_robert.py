e4c85fab9e17eaa0c573e9df97cd4899bc96f7b8
MartinMartimeo <martin@martimeo.de>
2011-05-20 14:06:27 +0200

    Small Bug Fix

--- src/interface/test_robert.py
+++ src/interface/test_robert.py
@@ -1,8 +1,8 @@
-from qt import *
+from PyQt4 import QtGui
 from ui import *
 import sys
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtGui.QApplication(sys.argv)
     f = Ui_MainWindow()
     f.show()
     app.setMainWidget(f)
