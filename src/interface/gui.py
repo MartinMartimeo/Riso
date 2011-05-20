cf4ed0d2fe9441ec448f045a35ee37339d6cdc4f
Robert Heumüller <robert@joshavg.de>
2011-05-20 18:36:36 +0200

    Scrolling / gtk.main_quit()

--- src/interface/gui.py
+++ src/interface/gui.py
@@ -4,14 +4,17 @@ from gtk import *
 class Gui:
     def __init__(self, writeFunc):
         self.writeFunc = writeFunc
-        self.main = Window()
+        self.main = Window(gtk.WINDOW_TOPLEVEL)
+        self.main.connect("destroy", lambda w: gtk.main_quit())
         self.layout = gtk.VBox()
         self.output = gtk.TextView()
         self.buffer = gtk.TextBuffer()
         self.output.set_buffer(self.buffer)
+        self.outputbox = gtk.ScrolledWindow()
+        self.outputbox.add(self.output)
         self.input = gtk.Entry()
         self.input.connect("activate", self.writeSocket)
-        self.layout.pack_start(self.output)
+        self.layout.pack_start(self.outputbox)
         self.layout.pack_start(self.input)
         self.main.add(self.layout)
         self.main.show_all()
@@ -21,7 +24,9 @@ class Gui:
         gtk.main()
         
     def write(self, text):
-        self.buffer.insert_at_cursor(text)
+        self.buffer.insert_at_cursor(str(text))
+        adj = self.outputbox.get_vadjustment()
+        adj.set_value(adj.get_upper())
         
     def writeSocket(self, data):
         text = self.input.get_text()
