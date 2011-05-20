fa7b0fad50c38192e2673da8e2d6868332cbf892
Robert Heumüller <robert@joshavg.de>
2011-05-20 14:14:11 +0200

    Maybe now

--- src/interface/test_robert.py
+++ src/interface/test_robert.py
@@ -1,9 +1,16 @@
-from qt import *
-from ui import *
 import sys
+from PyQt4 import QtCore, QtGui
+from ui import *
+
+class StartQT4(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    f = Ui_MainWindow()
-    f.show()
-    app.setMainWidget(f)
-    app.exec_loop()
+    app = QtGui.QApplication(sys.argv)
+    myapp = StartQT4()
+    myapp.show()
+    sys.exit(app.exec_())
