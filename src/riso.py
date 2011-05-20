bec7c3813dada3142c9b22b285fe4281a16148d6
MartinMartimeo <martin@martimeo.de>
2011-05-20 09:56:17 +0200

    Netbeans Initalisation

---
+++ src/riso.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Code By Severin Orth (MartinMartimeo)
+# martin@martimeo.de
+# Do not use without permission or care
+
+__author__  = "Severin <MartinMartimeo> Orth <martin@martimeo.de>"
+__date__    = "$20.05.2011 09:52:48$"
+
+if __name__ == "__main__":
+    print "Hello World"
