88cb8a348d0f0c61530df5fd54f024fc2bca53db
Robert Heumüller <robert@joshavg.de>
2011-05-21 17:43:36 +0200

    Farbanpassung moeglich

--- src/interface/gui.py
+++ src/interface/gui.py
@@ -11,9 +11,14 @@ class Gui:
         self.mainwindow = self.builder.get_object("window1")
         self.outputbox = self.builder.get_object("outputbox")
         self.output = self.builder.get_object("output")
+        self.bgcolor = self.builder.get_object("colorselection1")
+        self.textcolor = self.builder.get_object("colorselection2")
         self.input = self.builder.get_object("input")
         self.buffer = gtk.TextBuffer()
         self.output.set_buffer(self.buffer)
+        self.output.modify_text(gtk.STATE_NORMAL, gtk.gdk.color_parse("gray"))
+        self.output.modify_base(gtk.STATE_NORMAL, gtk.gdk.Color(0,0,0))
+        
         self.mainwindow.show_all()
         
 
@@ -34,6 +39,14 @@ class Gui:
         text = self.input.get_text()
         self.writeFunc(text)
         self.input.set_text("")
+        
+    def bg_color_changed(self, data):
+        color = self.bgcolor.get_current_color()
+        self.output.modify_base(gtk.STATE_NORMAL, color)
+        
+    def text_color_changed(self, data):
+        color = self.textcolor.get_current_color()
+        self.output.modify_text(gtk.STATE_NORMAL, color)
         
     
     
